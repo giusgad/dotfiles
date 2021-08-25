605be2e5c7201a255ac3e607a7a6620254bea2ff
giuseppegadola <giusgadola@gmail.com>
2021-08-25 17:55:33 +0200

    script to support more than two devices

--- .config/qtile/scripts/change_output.sh
+++ .config/qtile/scripts/change_output.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
-
 sinks=($(pacmd list-sinks | grep index | \
-    awk '{ if ($1 == "*") print "1",$3; else print "0",$2 }'))
+    awk '{ if ($1 == "*") print "1"; else print "0" }'))
 inputs=($(pacmd list-sink-inputs | grep index | awk '{print $2}'))
 
-[[ ${sinks[0]} = 0 ]] && swap=${sinks[1]} || swap=${sinks[3]}
+echo ${sinks[*]}
+#find active sink
+active=0
+for i in ${sinks[*]}
+do
+    if [ $i -eq 0 ]
+        then active=$((active+1))
+        else break
+    fi
+done
+
+#switch to next sink
+swap=$(((active+1)%${#sinks[@]}))
 
 pacmd set-default-sink $swap &> /dev/null
 for i in ${inputs[*]}; do pacmd move-sink-input $i $swap &> /dev/null; done
