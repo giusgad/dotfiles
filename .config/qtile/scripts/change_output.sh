50b0b8f5e3668906ddfd72c20e97bb8aeced6e23
giuseppegadola <giusgadola@gmail.com>
2021-09-02 17:17:52 +0200

    script now only switchs between two sources

--- .config/qtile/scripts/change_output.sh
+++ .config/qtile/scripts/change_output.sh
@@ -1,21 +1,88 @@
-#!/usr/bin/env bash
-sinks=($(pacmd list-sinks | grep index | \
-    awk '{ if ($1 == "*") print "1"; else print "0" }'))
-inputs=($(pacmd list-sink-inputs | grep index | awk '{print $2}'))
+CARD1="alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00"
+CARD2="alsa_output.pci-0000_0a_00.3"
 
-echo ${sinks[*]}
-#find active sink
-active=0
-for i in ${sinks[*]}
-do
-    if [ $i -eq 0 ]
-        then active=$((active+1))
-        else break
+CURRENT_SINK=$(pacmd stat | awk -F": " '/^Default sink name: /{print $2}' | awk 'BEGIN{FS=OFS="."} NF--' | sed 's/alsa_output/alsa_output/g')
+
+
+function setCard() {
+
+  if [ "$CURRENT_SINK" == "$1" ]
+   then
+     echo "Already using this Sink"
+     exit 1
+  fi
+
+  NEW_SINK=$(pacmd list-sinks | awk '/index:/ {print $1 $2 $3} /name:/ {print $0};' | grep -m1 -B1 $1 | grep index | awk '{print $1}' | cut -d ":" -f2)
+  SINK=$(pacmd set-default-sink $NEW_SINK)
+  INPUT=$(pacmd list-sink-inputs | grep index | awk '{print $2}')
+
+  pacmd move-sink-input $INPUT $NEW_SINK
+  echo "Moving input: $INPUT to sink: $NEW_SINK";
+  echo "Setting default sink to: $NEW_SINK";
+
+  notify-send --urgency=low "Audio Switching" "SINK: $NEW_SINK"
+}
+
+function toggleSinks() {
+  if [ "$CURRENT_SINK" == "$CARD1" ]
+    then
+      setCard $CARD2
+    else
+      setCard $CARD1
     fi
+}
+
+
+function showHelp() {
+  echo "------------------------------------"
+  echo "AUDIO SINK SWITCHER"
+  echo " "
+  echo "$0 [options]"
+  echo " "
+  echo "options:"
+  echo "-h  --help        What you are looking at.."
+  echo "-g, --gaming      Sets Gaming headset as output device"
+  echo "-s, --speakers    Sets Speakers as output device"
+  echo "-t, --toggle      Toggles the different output devices"
+  echo " "
+  echo "------------------------------------"
+}
+
+# check args length
+if [ $# -eq 0 ]
+  then
+    echo "Toggling output devices (Speakers/Headset)"
+    toggleSinks
+fi
+
+
+# arg options
+while test $# -gt 0; do
+    case "$1" in
+
+                -h|--help)
+                        showHelp
+                        exit 1
+                        ;;
+
+                -g|--gaming)
+                        setCard $CARD2
+                        exit 1
+                        ;;
+
+                -s|--speakers)
+                        setCard $CARD1
+                        exit 1
+                        ;;
+
+                -t|--toggle)
+                        toggleSinks
+                        echo "Toggling output devices (Speakers/Headset)"
+                        exit 1
+                        ;;
+                 *)
+                        showHelp
+                        exit 1
+                        ;;
+    esac
 done
-
-#switch to next sink
-swap=$(((active+1)%${#sinks[@]}))
-
-pacmd set-default-sink $swap &> /dev/null
-for i in ${inputs[*]}; do pacmd move-sink-input $i $swap &> /dev/null; done
