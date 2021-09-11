a673234865e1ba168ee7989e84bf6cc3a5d08c65
giuseppegadola <giusgadola@gmail.com>
2021-09-11 17:13:05 +0200

    changed picom fork

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -21,7 +21,8 @@ run xfce4-power-manager &
 run volumeicon &
 numlockx on &
 #blueberry-tray &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+# picom --config $HOME/.config/qtile/scripts/picom.conf &
+picom --config $HOME/.config/qtile/scripts/picom-jonaburg.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
