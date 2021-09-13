381b6fb94e7e4398ac51730a3798dbc2d7f44b59
giuseppegadola <giusgadola@gmail.com>
2021-09-13 20:05:47 +0200

    added live wallpaper script

---
+++ .config/qtile/scripts/live_wallpaper.sh
@@ -0,0 +1,19 @@
+_screen() {
+    xwinwrap -ov -g $1 -- mpv --fullscreen\
+	--hwdec=vdpau --vo=gpu \
+        --on-all-workspaces --no-keepaspect-window \
+        --no-stop-screensaver --no-input-default-bindings\
+	--no-sub --no-window-dragging \
+	--panscan=1.0 \
+        --loop-file --no-audio --no-osc --no-osd-bar -wid WID --quiet \
+        "$2" --background="$3" --video-zoom="$4" &
+}
+
+killall xwinwrap
+
+sleep 0.5
+
+for i in $( xrandr -q | grep ' connected' | grep -oP '\d+x\d+\+\d+\+\d+')
+do
+    _screen $i "$1" "${2:-#000000}" "${3:-0}"
+done
