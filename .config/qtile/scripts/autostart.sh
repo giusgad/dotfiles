cb9d582c6bb484e00ab049de722575a20f796e63
giuseppegadola <giusgadola@gmail.com>
2021-09-14 13:12:45 +0200

    autostart live wallpaper

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -12,7 +12,8 @@ run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
 #starting utility applications at boot time
 #run variety &
-nitrogen --restore
+#nitrogen --restore
+sh .config/qtile/scripts/live_wallpaper.sh /mnt/MyBook/wallpapers/video/nobara_roses.mp4
 run caffeine &
 run nm-applet &
 run redshift-gtk -l 46.4:9.35 &
@@ -21,7 +22,7 @@ run xfce4-power-manager &
 run volumeicon &
 numlockx on &
 #blueberry-tray &
-# picom --config $HOME/.config/qtile/scripts/picom.conf &
+#picom --config $HOME/.config/qtile/scripts/picom.conf &
 picom --config $HOME/.config/qtile/scripts/picom-jonaburg.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
