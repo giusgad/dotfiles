6fe2e22cb1f18628aedda9c307fd1fe5a307bc2d
giuseppegadola <giusgadola@gmail.com>
2021-09-19 11:24:23 +0200

    trying different performance parameters

--- .config/qtile/scripts/live_wallpaper.sh
+++ .config/qtile/scripts/live_wallpaper.sh
@@ -1,6 +1,6 @@
 _screen() {
     xwinwrap -ov -g $1 -- mpv --fullscreen\
-	--hwdec=vdpau --vo=vdpau \
+	--hwdec=auto --vo=gpu \
         --on-all-workspaces --no-keepaspect-window \
         --no-stop-screensaver --no-input-default-bindings \
 	--no-sub --no-window-dragging \
