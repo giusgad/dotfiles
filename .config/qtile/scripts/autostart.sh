ad9fef206422e240b0b78d718dee2c1f78d7a1f4
Giuseppe Gadola <giusgadola@gmail.com>
2022-07-01 13:27:19 +0200

    autostart unclutter

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -26,6 +26,7 @@ picom --experimental-backends &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 #/usr/lib/xfce4/notifyd/xfce4-notifyd &
 /usr/bin/dunst &
+unclutter --hide-on-touch &
 
 #starting user applications at boot time
 run kdeconnect-indicator &
