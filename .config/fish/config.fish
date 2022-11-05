0e5f2fb06da410335a594f5b48ea4f9d9792b055
Giuseppe Gadola <giusgadola@gmail.com>
2022-11-05 16:49:23 +0100

    cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
 # set custom keybindings
-bind -k nul accept-autosuggestion
+bind -k nul -m insert accept-autosuggestion
 bind \b backward-kill-word
 bind \e\[3\;5~ kill-word
 
 # basic settings (export)
 set fish_greeting
-set TERM "alacritty"
+set TERM "kitty"
 set EDITOR "nvim"
 set VISUAL "nvim"
 
@@ -18,11 +18,9 @@ set fish_color_param yellow
 
 # SET EITHER DEFAULT EMACS MODE OR VI MODE
 function fish_user_key_bindings
-  fish_default_key_bindings
-  # fish_vi_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
 end
-
-# get aliases to work with sudo
 
 # bash like ! and !!
 function __history_previous_command
@@ -64,7 +62,6 @@ eval /home/giuseppe/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 
 # Run fetch tool at startup in interactive session
 if status is-interactive
-    # commands for interactive sessions
     # neofetch
     /home/giuseppe/.scripts/fetch.sh
 end
@@ -82,33 +79,6 @@ starship init fish | source
 if [ -d "$HOME/.local/bin" ]
     set PATH "$HOME/.local/bin:$PATH"
 end
-
-# GNOME-KEYRING
-#if test -n "$DESKTOP_SESSION"
-#    set -x (gnome-keyring-daemon --start | string split "=")
-#end
-
-#set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-#
-#pgrep dbus-daemon > /dev/null
-#
-#if test $status -eq 1
-#  dbus-launch --sh-syntax | read --line bus_address ignored bus_pid bus_windowid
-#
-#  set -Ux DBUS_SESSION_BUS_ADDRESS (string match -r "'(.*)'" $bus_address)[2]
-#  set -Ux DBUS_SESSION_BUS_ID (string match -r "=(.*);" $bus_pid)[2]
-#  set -Ux DBUS_SESSION_BUS_WINDOWID (string match -r "=(.*);" $bus_windowid)[2]
-#end
-#
-## pgrep limited to 15 chars, so truncate `daemon`
-#pgrep gnome-keyring-d > /dev/null
-#
-#if test $status -eq 1
-#  gnome-keyring-daemon | read --line gnome_keyring_control ssh_auth_sock
-#
-#  set -Ux GNOME_KEYRING_CONTROL (string split -m 1 = $gnome_keyring_control)[2]
-#  set -Ux SSH_AUTH_SOCK (string split -m 1 = $ssh_auth_sock)[2]
-#end
 
 # ssh
 eval "$(ssh-agent -c)" > /dev/null
