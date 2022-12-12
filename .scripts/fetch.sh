ecd0965a801d1196f475a6ce10b3d876b62a2637
Giuseppe Gadola <giusgadola@gmail.com>
2022-12-12 10:18:54 +0100

    random fetch selected

--- .scripts/fetch.sh
+++ .scripts/fetch.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 # run neofetch or pfetch based on the width of the terminal
 # the width is calculated with the output of "tput cols"
 
 if [ $(tput cols) -le 100 ] || [ $(tput lines) -le 30 ]; then
-    pfetch
+    if [ -f "/bin/pfetch" ] ; then
+        array[0]=pfetch
+    fi
+    if [ -f "/bin/sfetch" ] ; then
+        array[1]=sfetch
+    fi
+    size=${#array[@]}
+    index=$(($RANDOM % $size))
+    exec ${array[$index]}
 else
     neofetch
 fi
