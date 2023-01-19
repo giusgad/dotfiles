32e9050369c95dca2b0c7db66d91bcbf18445255
Giuseppe Gadola <giusgadola@gmail.com>
2023-01-19 18:42:09 +0100

    rounded lualine

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -1,58 +1,46 @@
 local lualine_c = { "filename" }
 local ok, noice = pcall(require, "noice")
 if ok then
-    lualine_c = { -- show macro recording
-        "filename",
-        {
-            noice.api.statusline.mode.get,
-            cond = noice.api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-        },
-    }
+	lualine_c = { -- show macro recording
+		"filename",
+		{
+			noice.api.statusline.mode.get,
+			cond = noice.api.statusline.mode.has,
+			color = { fg = "#ff9e64" },
+		},
+	}
 end
 
 local lualine_ok, lualine = pcall(require, "lualine")
 if not lualine_ok then
-    return
+	return
 end
 
 lualine.setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        },
-    },
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = lualine_c,
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_a = { { "mode", separator = { left = "", right = "" } } },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = lualine_c,
+		lualine_x = {},
+		lualine_y = { "filetype", "progress" },
+		lualine_z = { { "location", separator = { left = "", right = "" } } },
+	},
+	inactive_sections = {
+		lualine_a = { "filename" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
 })
