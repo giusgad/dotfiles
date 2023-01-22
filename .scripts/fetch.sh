b9a9bd2d4729263eead2252c88ac35cfc52d17eb
Giuseppe Gadola <giusgadola@gmail.com>
2023-01-22 23:10:00 +0100

    small refactoring

--- .scripts/fetch.sh
+++ .scripts/fetch.sh
@@ -16,36 +16,29 @@ function small {
     exec ${array[$index]}
 }
 
+# medium fetches
+if [ -f "/bin/nitch" ] ; then
+    arr+=("nitch")
+fi
+if [ -f "/bin/rxfetch" ] ; then
+    arr+=("rxfetch")
+fi
+arr+=("small")
+
 if [ $(tput cols) -le 100 ]; then
     if [ $(tput lines) -le 30 ]; then
         small
     else
-        # medium fetches
-        if [ -f "/bin/nitch" ] ; then
-            arr+=("nitch")
-        fi
-        if [ -f "/bin/rxfetch" ] ; then
-            arr+=("rxfetch")
-        fi
-        arr+=("small")
         size=${#arr[@]}
         index=$(($RANDOM % $size))
         ${arr[$index]}
     fi
 else
-    arr[0]=small
     # big fetch
     if [ -f /bin/fastfetch ] ; then
-        arr[1]=fastfetch
+        arr+=("fastfetch")
     elif [ -f /bin/neofetch ] ; then
-        arr[1]=neofetch
-    fi
-    # medium fetches
-    if [ -f "/bin/nitch" ] ; then
-        arr+=("nitch")
-    fi
-    if [ -f "/bin/rxfetch" ] ; then
-        arr+=("rxfetch")
+        arr+=("neofetch")
     fi
     size=${#arr[@]}
     index=$(($RANDOM % $size))
