9699433a335df6761a4c8a9610194a403a2c99db
giuseppegadola <giusgadola@gmail.com>
2023-04-15 13:17:27 +0200

    fix2

--- .scripts/kitty_autocd.sh
+++ .scripts/kitty_autocd.sh
@@ -1,3 +1,8 @@
+windowclass=$(hyprctl activewindow | grep class | awk '{print $2}')
+if [ "$windowclass" != kitty ]; then
+    echo $HOME
+    exit
+fi
 kitty_pid=$(hyprctl activewindow | grep pid | awk '{print $2}')
 fish_pid=$(ps --ppid "$kitty_pid" -o pid=)
 fish_pid=$(echo $fish_pid | awk '{print $2}')
