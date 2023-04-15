1dae4b0123905c3af0a4c1786d810b1889f18424
giusgad <giusgadola@gmail.com>
2023-04-15 12:01:40 +0200

    yes

--- .scripts/kitty_autocd.sh
+++ .scripts/kitty_autocd.sh
@@ -2,4 +2,7 @@ kitty_pid=$(hyprctl activewindow | grep pid | awk '{print $2}')
 fish_pid=$(ps --ppid "$kitty_pid" -o pid=)
 fish_pid=$(echo $fish_pid | awk '{print $2}')
 dir=$(pwdx $fish_pid | awk '{print $2}')
+if [ "$dir" = "" ]; then
+    dir=$HOME
+fi
 echo $dir
