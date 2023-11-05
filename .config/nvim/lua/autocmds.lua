7a345481080e92139835525317a3657ece4f9b80
giuseppegadola <giusgadola@gmail.com>
2023-11-05 12:07:05 +0100

    nvim wgsl filetype

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,26 +1,33 @@
 -- indentation to 2 spaces
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.scss", "*.css", "*.ts*", "*.js*", "*.json", "*.java" },
-    callback = function()
-        vim.bo.shiftwidth = 2
-        vim.bo.tabstop = 2
-        vim.bo.expandtab = false
-    end,
+	pattern = { "*.scss", "*.css", "*.ts*", "*.js*", "*.json", "*.java" },
+	callback = function()
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+		vim.bo.expandtab = false
+	end,
 })
 -- open help in vsplit
 vim.api.nvim_create_autocmd({ "BufWinEnter", "Bufenter" }, {
-    callback = function()
-        if vim.bo.filetype == "help" or vim.bo.filetype == "fugitive" then
-            vim.cmd.wincmd("L")
-        end
-    end,
+	callback = function()
+		if vim.bo.filetype == "help" or vim.bo.filetype == "fugitive" then
+			vim.cmd.wincmd("L")
+		end
+	end,
 })
 
 -- check for changes when leaving terminal (for lazygit)
 vim.api.nvim_create_autocmd("TermLeave", {
-    callback = function()
-        if not vim.api.nvim_buf_get_option(vim.api.nvim_get_current_buf(), "modified") then
-            vim.cmd("e")
-        end
-    end,
+	callback = function()
+		if not vim.api.nvim_buf_get_option(vim.api.nvim_get_current_buf(), "modified") then
+			vim.cmd("e")
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = "*.wgsl",
+	callback = function()
+		vim.bo.filetype = "wgsl"
+	end,
 })
