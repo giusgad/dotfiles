69a38679f858414004bc14ccb0ca56177fe1aa84
giusgad <67549788+giusgad@users.noreply.github.com>
2023-12-22 17:03:38 +0100

    nvim oil and stylua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,5 +1,5 @@
 local function map(mode, lhs, rhs, desc)
-    vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+	vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
 end
 
 -- normal_mode = "n", insert_mode = "i", visual_mode = "v", visual_block_mode = "x", term_mode = "t", command_mode = "c",
@@ -39,7 +39,7 @@ map({ "n", "v" }, "<leader>D", '"_D')
 -- add undo points while typing
 local chars = { "<Space>", ",", ".", "(", "[", "{", ":", ";" }
 for _, c in ipairs(chars) do
-    map("i", c, "<C-g>u" .. c)
+	map("i", c, "<C-g>u" .. c)
 end
 
 -- VISUAL MODE
@@ -77,9 +77,9 @@ map("n", "<leader>vi", ":lua vim.lsp.buf.implementation()<CR> zz", "lsp go to [I
 map("n", "<leader>f<space>", ":lua vim.lsp.buf.format()<CR>", "lsp format")
 map("n", "<leader>vf", ":lua vim.lsp.buf.format()<CR>", "lsp [F]ormat")
 map("n", "<leader>vh", function()
-    local enabled = vim.lsp.inlay_hint.is_enabled()
-    vim.lsp.inlay_hint.enable(0, not enabled)
-    vim.notify("inlay hints enabled: " .. tostring(not enabled), vim.log.levels.INFO, { title = "LSP" })
+	local enabled = vim.lsp.inlay_hint.is_enabled()
+	vim.lsp.inlay_hint.enable(0, not enabled)
+	vim.notify("inlay hints enabled: " .. tostring(not enabled), vim.log.levels.INFO, { title = "LSP" })
 end, "lsp show inlay [H]ints")
 -- map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>",opts)
 -- map("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>",opts)
@@ -94,86 +94,86 @@ map("n", "<leader>td", ":bdelete<CR>", "delete tab")
 -- HARPOON
 local ok, mark = pcall(require, "harpoon.mark")
 if ok then
-    local ui = require("harpoon.ui")
-    map("n", "<leader>a", mark.add_file, "add to harpoon")
-    map("n", "<C-f>", ui.toggle_quick_menu)
+	local ui = require("harpoon.ui")
+	map("n", "<leader>a", mark.add_file, "add to harpoon")
+	map("n", "<C-f>", ui.toggle_quick_menu)
     -- stylua: ignore start
     map("n", "<leader>h", function() ui.nav_file(1) end, "harpoon jump")
     map("n", "<leader>j", function() ui.nav_file(2) end, "harpoon jump")
     map("n", "<leader>k", function() ui.nav_file(3) end, "harpoon jump")
     map("n", "<leader>l", function() ui.nav_file(4) end, "harpoon jump")
-    -- stylua: ignore end
+	-- stylua: ignore end
 end
 -- NOTIFY
 local notify_ok, notify = pcall(require, "notify")
 if notify_ok then
-    map({ "n", "v" }, "<leader>n", notify.dismiss)
+	map({ "n", "v" }, "<leader>n", notify.dismiss)
 end
 -- LSPSAGA
 local saga_ok, _ = pcall(require, "lspsaga")
 if saga_ok then
-    map("n", "<leader>vu", "<cmd>Lspsaga finder<CR>", "lsp show references ([U]sed)") -- view all options
-    map({ "n", "v" }, "<leader>va", "<cmd>Lspsaga code_action<CR>", "lsp code [A]ction") -- code action
-    map("n", "<leader>vr", "<cmd>Lspsaga rename<CR>", "lsp [R]ename") -- rename
-    map("n", "<leader>vD", "<cmd>Lspsaga peek_definition<CR>", "lsp peek [D]efinition") -- definition in floating window
-    map("n", "<leader>vl", "<cmd>Lspsaga show_line_diagnostics<CR>", "lsp [L]ine diagnostics") -- show line diagnostics
-    map("n", "<leader>vc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", "lsp [C]ursor diagnostics") -- show cursor diagnostic
-    map("n", "<leader>vo", "<cmd>Lspsaga outline<CR>", "lsp [O]utline") -- outline
-    map("n", "K", "<cmd>Lspsaga hover_doc<CR>", "hover doc") -- hover docs
-    map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", "prev diagnostic") -- jump to diagnostics
-    map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", "next diagnostic") -- jump to diagnostics
-    map("n", "[E", function() -- jump to errors
-        require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-    end, "prev error")
-    map("n", "]E", function() -- jump to errors
-        require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-    end, "next error")
+	map("n", "<leader>vu", "<cmd>Lspsaga finder<CR>", "lsp show references ([U]sed)") -- view all options
+	map({ "n", "v" }, "<leader>va", "<cmd>Lspsaga code_action<CR>", "lsp code [A]ction") -- code action
+	map("n", "<leader>vr", "<cmd>Lspsaga rename<CR>", "lsp [R]ename") -- rename
+	map("n", "<leader>vD", "<cmd>Lspsaga peek_definition<CR>", "lsp peek [D]efinition") -- definition in floating window
+	map("n", "<leader>vl", "<cmd>Lspsaga show_line_diagnostics<CR>", "lsp [L]ine diagnostics") -- show line diagnostics
+	map("n", "<leader>vc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", "lsp [C]ursor diagnostics") -- show cursor diagnostic
+	map("n", "<leader>vo", "<cmd>Lspsaga outline<CR>", "lsp [O]utline") -- outline
+	map("n", "K", "<cmd>Lspsaga hover_doc<CR>", "hover doc") -- hover docs
+	map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", "prev diagnostic") -- jump to diagnostics
+	map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", "next diagnostic") -- jump to diagnostics
+	map("n", "[E", function() -- jump to errors
+		require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	end, "prev error")
+	map("n", "]E", function() -- jump to errors
+		require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+	end, "next error")
 end
 
 -- GITSIGNS
 local gs_ok, gs = pcall(require, "gitsigns")
 if gs_ok then
-    map("n", "<leader>gp", gs.preview_hunk, "git [P]review hunk")
-    map("n", "<leader>gd", gs.diffthis, "git [D]iff")
-    map("n", "<leader>gD", function()
-        local branch = vim.fn.input({ prompt = "branch to diff against:", default = "main", cancelreturn = "main" })
-        gs.diffthis(branch)
-    end, "git [D]iff on custom branch")
-    map("n", "<leader>gr", gs.reset_hunk, "git [R]eset hunk")
-    map("n", "<leader>gR", gs.reset_buffer, "git [R]eset buffer")
-    map("n", "<leader>gh", gs.stage_hunk, "git [S]tage hunk")
-    map("n", "<leader>gs", gs.stage_buffer, "git [S]tage buffer")
-    map("n", "<leader>gv", gs.toggle_deleted, "git [V]iew deleted")
-    map("n", "[h", gs.prev_hunk, "git prev hunk")
-    map("n", "]h", gs.next_hunk, "git next hunk")
+	map("n", "<leader>gp", gs.preview_hunk, "git [P]review hunk")
+	map("n", "<leader>gd", gs.diffthis, "git [D]iff")
+	map("n", "<leader>gD", function()
+		local branch = vim.fn.input({ prompt = "branch to diff against:", default = "main", cancelreturn = "main" })
+		gs.diffthis(branch)
+	end, "git [D]iff on custom branch")
+	map("n", "<leader>gr", gs.reset_hunk, "git [R]eset hunk")
+	map("n", "<leader>gR", gs.reset_buffer, "git [R]eset buffer")
+	map("n", "<leader>gh", gs.stage_hunk, "git [S]tage hunk")
+	map("n", "<leader>gs", gs.stage_buffer, "git [S]tage buffer")
+	map("n", "<leader>gv", gs.toggle_deleted, "git [V]iew deleted")
+	map("n", "[h", gs.prev_hunk, "git prev hunk")
+	map("n", "]h", gs.next_hunk, "git next hunk")
 end
 -- TROUBLE
 local trouble_ok, _ = pcall(require, "trouble")
 if trouble_ok then
-    map("n", "<leader>vt", require("trouble").toggle, "lsp show all errors ([T]rouble)")
+	map("n", "<leader>vt", require("trouble").toggle, "lsp show all errors ([T]rouble)")
 end
 
 -- TELESCOPE
 local ok, builtin = pcall(require, "telescope.builtin")
 if ok then
-    map("n", "<leader>ff", builtin.find_files, "telescope [F]ind [F]iles")
-    map("n", "<leader>fb", ":Telescope file_browser<CR>", "telescope [F]ile [B]rowser")
-    map("n", "<leader>fe", builtin.diagnostics, "telescope diagnostics ([E]rrors)")
-    map("n", "<leader>fE", function()
-        builtin.diagnostics({ bufnr = 0 })
-    end, "telescope buffer diagnostics")
-    map("n", "<leader>fg", builtin.live_grep, "telescope [G]rep")
-    map("n", "<leader>fh", builtin.buffers, "telescope buffers ([H]arpoon)")
-    map("n", "<leader>fn", ":Telescope notify<CR>", "telescope [N]otifications")
-    map("n", "<leader>ft", ":TodoTelescope<CR>", "telescope [T]ODOs")
-    -- worktrees
-    map("n", "<leader>gtl", require("telescope").extensions.git_worktree.git_worktrees, "git work[T]ree [L]ist/switch")
-    map(
-        "n",
-        "<leader>gtn",
-        require("telescope").extensions.git_worktree.create_git_worktree,
-        "git work[T]ree add/[N]ew"
-    )
+	map("n", "<leader>ff", builtin.find_files, "telescope [F]ind [F]iles")
+	map("n", "<leader>fb", ":Telescope file_browser<CR>", "telescope [F]ile [B]rowser")
+	map("n", "<leader>fe", builtin.diagnostics, "telescope diagnostics ([E]rrors)")
+	map("n", "<leader>fE", function()
+		builtin.diagnostics({ bufnr = 0 })
+	end, "telescope buffer diagnostics")
+	map("n", "<leader>fg", builtin.live_grep, "telescope [G]rep")
+	map("n", "<leader>fh", builtin.buffers, "telescope buffers ([H]arpoon)")
+	map("n", "<leader>fn", ":Telescope notify<CR>", "telescope [N]otifications")
+	map("n", "<leader>ft", ":TodoTelescope<CR>", "telescope [T]ODOs")
+	-- worktrees
+	map("n", "<leader>gtl", require("telescope").extensions.git_worktree.git_worktrees, "git work[T]ree [L]ist/switch")
+	map(
+		"n",
+		"<leader>gtn",
+		require("telescope").extensions.git_worktree.create_git_worktree,
+		"git work[T]ree add/[N]ew"
+	)
 end
 
 -- DAP
@@ -181,24 +181,24 @@ local dap_ok, dap = pcall(require, "dap")
 local dapui_ok, dapui = pcall(require, "dapui")
 local persist_bp_ok, persist_bp = pcall(require, "persistent-breakpoints.api")
 if persist_bp_ok then
-    map("n", "<leader>bb", persist_bp.toggle_breakpoint, "dap toggle [B]reakpoint")
-    map("n", "<leader>bB", persist_bp.set_conditional_breakpoint, "dap conditional [B]reakpoint")
-    map("n", "<leader>bR", persist_bp.clear_all_breakpoints, "dap [R]emove all [B]reakpoints")
+	map("n", "<leader>bb", persist_bp.toggle_breakpoint, "dap toggle [B]reakpoint")
+	map("n", "<leader>bB", persist_bp.set_conditional_breakpoint, "dap conditional [B]reakpoint")
+	map("n", "<leader>bR", persist_bp.clear_all_breakpoints, "dap [R]emove all [B]reakpoints")
 end
 if dapui_ok then
-    map("n", "<leader>bv", dapui.toggle)
+	map("n", "<leader>bv", dapui.toggle)
 end
 if dap_ok then
-    map("n", "<leader>bc", dap.continue, "dap [C]ontinue")
-    map("n", "<leader>bi", dap.step_into, "dap step [I]nto")
-    map("n", "<leader>bo", dap.step_over, "dap step [O]ver")
-    map("n", "<leader>bu", dap.step_out, "dap step o[U]t")
+	map("n", "<leader>bc", dap.continue, "dap [C]ontinue")
+	map("n", "<leader>bi", dap.step_into, "dap step [I]nto")
+	map("n", "<leader>bo", dap.step_over, "dap step [O]ver")
+	map("n", "<leader>bu", dap.step_out, "dap step o[U]t")
 end
 -- dap-go
 local dap_go_ok, dap_go = pcall(require, "dap-go")
 if vim.fn.expand("%:e") == "go" and dap_go_ok then
-    vim.keymap.set("n", "<leader>bt", dap_go.debug_test, buffer_opts)
-    vim.keymap.set("n", "<leader>bT", dap_go.debug_test, buffer_opts)
+	vim.keymap.set("n", "<leader>bt", dap_go.debug_test, buffer_opts)
+	vim.keymap.set("n", "<leader>bT", dap_go.debug_test, buffer_opts)
 end
 
 -- UNDOTREE
@@ -208,20 +208,20 @@ map("n", "<leader>u", vim.cmd.UndotreeToggle, "undo tree")
 -- <M-d> to toggle
 local term_ok, term = pcall(require, "toggleterm.terminal") -- terminal for lazygit
 if term_ok then
-    term = term.Terminal
-    -- lazygit terminal
-    local lazygit = term:new({ cmd = "lazygit", direction = "float", hidden = true })
-    local function _lazygit_toggle()
-        lazygit:toggle()
-    end
+	term = term.Terminal
+	-- lazygit terminal
+	local lazygit = term:new({ cmd = "lazygit", direction = "float", hidden = true })
+	local function _lazygit_toggle()
+		lazygit:toggle()
+	end
 
-    map("n", "<leader>gg", _lazygit_toggle, "lazygit term")
-    map("t", "<M-k>", _lazygit_toggle)
+	map("n", "<leader>gg", _lazygit_toggle, "lazygit term")
+	map("t", "<M-k>", _lazygit_toggle)
 
-    -- numbered terminals
-    for i = 1, 10, 1 do -- create separate terminals
-        map("n", "<leader>g" .. i, ":" .. i .. "ToggleTerm<CR>", "open new term id:" .. i)
-    end
+	-- numbered terminals
+	for i = 1, 10, 1 do -- create separate terminals
+		map("n", "<leader>g" .. i, ":" .. i .. "ToggleTerm<CR>", "open new term id:" .. i)
+	end
 end
 
 -- SESSION MANAGER
@@ -230,40 +230,40 @@ map("n", "<leader>sl", ":SessionManager load_session<CR>", "[S]ession [L]oad")
 -- BUFFERLINE
 local bufferline_ok = pcall(require, "bufferline")
 if bufferline_ok then
-    map("n", "<S-h>", ":BufferLineCyclePrev<CR>")
-    map("n", "<S-l>", ":BufferLineCycleNext<CR>")
-    map("n", "<leader>th", ":BufferLineMovePrev<CR>")
-    map("n", "<leader>tl", ":BufferLineMoveNext<CR>")
+	map("n", "<S-h>", ":BufferLineCyclePrev<CR>")
+	map("n", "<S-l>", ":BufferLineCycleNext<CR>")
+	map("n", "<leader>th", ":BufferLineMovePrev<CR>")
+	map("n", "<leader>tl", ":BufferLineMoveNext<CR>")
 end
 
 -- DIAL
 local dial_ok, dial = pcall(require, "dial.map")
 if dial_ok then
-    map("n", "<C-a>", dial.inc_normal())
-    map("n", "<C-x>", dial.dec_normal())
-    map("v", "<C-a>", dial.inc_visual())
-    map("v", "<C-x>", dial.dec_visual())
-    map("v", "g<C-a>", dial.inc_gvisual())
-    map("v", "g<C-x>", dial.dec_gvisual())
+	map("n", "<C-a>", dial.inc_normal())
+	map("n", "<C-x>", dial.dec_normal())
+	map("v", "<C-a>", dial.inc_visual())
+	map("v", "<C-x>", dial.dec_visual())
+	map("v", "g<C-a>", dial.inc_gvisual())
+	map("v", "g<C-x>", dial.dec_gvisual())
 end
 
 -- PETS
 local pets_ok = pcall(require, "pets")
 if pets_ok then
-    map("n", "<leader>PP", ":PetsPauseToggle<CR>")
-    map("n", "<leader>PH", ":PetsHideToggle<CR>")
-    map("n", "<leader>PS", ":PetsSleepToggle<CR>")
-    map("n", "<leader>PN", ":PetsNew " .. "gino" .. "<CR>")
+	map("n", "<leader>PP", ":PetsPauseToggle<CR>")
+	map("n", "<leader>PH", ":PetsHideToggle<CR>")
+	map("n", "<leader>PS", ":PetsSleepToggle<CR>")
+	map("n", "<leader>PN", ":PetsNew " .. "gino" .. "<CR>")
 end
 
 -- NABLA
 local nabla_ok, nabla = pcall(require, "nabla")
 if nabla_ok then
-    map("n", "<leader>mp", nabla.popup)
-    map("n", "<leader>mv", nabla.toggle_virt)
+	map("n", "<leader>mp", nabla.popup)
+	map("n", "<leader>mv", nabla.toggle_virt)
 end
 
-local oil_ok = pcall(require,"oil")
+local oil_ok, oil = pcall(require, "oil")
 if oil_ok then
-    map("n", "<leader>o", ":Oil<CR>")
+	map("n", "<leader>o", oil.toggle_float)
 end
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -139,7 +139,11 @@ require("lazy").setup({
 
 	{
 		"stevearc/oil.nvim",
-		opts = {},
+		opts = {
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = { show_hidden = true },
+		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 
