2975a3dd0e5213c63074595ad3dd6037239296f1
giusgad <67549788+giusgad@users.noreply.github.com>
2024-03-11 18:43:20 +0100

    swww hyprland wallpaper

--- .scripts/swww_wallpaper.sh
+++ .scripts/swww_wallpaper.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
-	echo "Usage:
-	$0 <dir containing images>"
-	exit 1
-fi
+function randomInDir() {
+    if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
+        echo "Usage:
+        $0 <dir containing images>"
+        exit 1
+    fi
 
-INTERVAL=600
+    INTERVAL=600
 
-while true; do
-	find "$1" -type f \
-		| while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img "$img" -t random
-			sleep $INTERVAL
-		done
+    while true; do
+        find "$1" -type f \
+            | while read -r img; do
+                echo "$((RANDOM % 1000)):$img"
+            done \
+            | sort -n | cut -d':' -f2- \
+            | while read -r img; do
+                swww img "$img" -t random
+                sleep $INTERVAL
+            done
+    done
+}
+
+swww init > /dev/null
+
+while swww init&>/dev/null; do
+    sleep 1
 done
+
+randomInDir $1
